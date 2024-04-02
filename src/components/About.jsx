1c7e395ad249dc6ce1493ae478694693fc9a601e
ziaxgit <112583896+ziaxgit@users.noreply.github.com>
2024-04-02 03:44:18 +0100

    update README

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,7 +1,7 @@
 export default function About() {
   return (
     <p className="about-section">
-      Welcome to Zia's News, where information meets engagement. This is more
+      Welcome to the News Room, where information meets engagement. This is more
       than just a news platfrom - this is a dynamic community-driven space where
       voices converge, opinions fourish, and discussions thrive.
     </p>
